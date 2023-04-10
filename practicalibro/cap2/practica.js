c515c5c98dca5ec7f0bb72f895d2e8d4f8c099ca
Melina Bustos <melina.m.bustos@gmail.com>
2023-04-10 18:10:06 -0300

    10-4-23-18:10

---
+++ practicalibro/cap2/practica.js
@@ -0,0 +1,23 @@
+let deuda = 100;
+deuda = deuda - 50;
+deuda = deuda + 10;
+// console.log(deuda);
+
+let uno = 1, dos = 2, tres = 3;
+// console.log(uno + tres);
+
+// console.log(Math.max(2, 5));
+
+// console.log(Math.min(2, 4) + 100);
+
+// funcion de control en linea recta
+// let elNumero = Number(promt("Elige el numero"));
+// console.log("tu numero es la raiz cuadrada de" + elNumero * elNumero);
+
+// funcion de ejecucion condicional
+// let elNumero = Number(promt("Elige el numero"));
+// if (!Number.isNaN(elNumber)) {
+//     console.log("tu numero es la raiz cuadrada de" + elNumero * elNumero);
+// }
+
+if (1 + 1 !== 3) console.log("es falso");
