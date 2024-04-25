89a5a4f87a25caa1d02b411eed9df329fad49ccc
Melina Bustos <melina.m.bustos@gmail.com>
2024-04-25 18:17:19 -0300

    25-4

---
+++ practicalibro/web/aulascript/condicionales1.js
@@ -0,0 +1,17 @@
+//ejercicio 1
+/*
+let numero = 4;
+let resto = `${numero % 2}` ;
+
+if (resto == 0) {
+
+    console.log("es par")
+
+}else{
+    console.log("es impar")
+};
+*/
+
+// ejercicio 2
+
+
---
+++ practicalibro/web/aulascript/variables.js
@@ -0,0 +1,46 @@
+/*https://www.aulascript.com/evaluar/ejercicios_basicos_1.htm*/
+// ejercicio 1
+/*
+let precio = 200;
+let iva = 21;
+
+let total =  `${precio + iva*precio/ 100}`;
+
+console.log(total);
+*/
+
+// ejercicio 2
+/*
+let lado = 40;
+let area = `${lado * lado}`;
+let perimetro = `${lado * 4}`;
+console.log(`area:${area} + perimetro:${perimetro}`);
+*/
+
+// ejercicio 3
+/*
+let usuario = prompt("Ingrese el nombre");
+let saludo = `hola ${usuario}`;
+console.log(saludo);
+*/
+
+//ejercicio 4
+/*
+let numero1 = prompt("ingrese numero1");
+let numero2 = prompt("ingrese numero2");
+let numero3 = prompt("ingrese numero3");
+let suma = `${numero1 + numero2 + numero3}`;
+let media = `${suma/3}`;
+console.log(media);
+*/
+
+// ejercicio 5
+/*
+cantidadDeKilometros = prompt("ingresar cantidad de kilometros recorridos");
+cantidadDeLitrosDeCombustible = prompt("ingrese cantidad de litros consumidos");
+consumoDeCombustible = `${cantidadDeLitrosDeCombustible/cantidadDeKilometros}`;
+*/
+
+
+
+
